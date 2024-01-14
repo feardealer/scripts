24c5b2e199ec29f7dc02dc09f450c553459b49b0
Vadim <stylish_bear@icloud.com>
2024-01-14 13:29:35 +0400

    add docs into the script

--- space_alert.py
+++ space_alert.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python3
 
+"""
+Author: Vadim Belous
+Email: fancybear@internet.ru 
+
+The main goal of this script is to send notifications to the chat
+about the amount of free disk space where this script is running
+
+Dependencies:
+pip install psutil python-telegram-bot
+
+Example Usage:
+python space_alert.py
+
+Cron job example:
+0 * * * * python3 /root/space_alert.py
+"""
+
 import asyncio
 import psutil
 import subprocess
 from telegram import Bot
+
+
+
+"""
+1. Go to the BotFather and create bot. U will receive a token to access to the http api. Paste ur token to BOT_TOKEN
+2. Go to https://api.telegram.org/botX/getUpdates when X - is ur token. U will see chat id. Paste this value to the CHAT_ID
+
+For example, send message via curl:
+curl -X POST "https://api.telegram.org/botX/sendMessage" -d "chat_id=Y&text=Z"
+When:
+    X - Token
+    Y - Chat id
+    Z - Message
+
+"""
+
 
 BOT_TOKEN = ''
 CHAT_ID = ''
