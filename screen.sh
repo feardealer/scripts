4a3b8a9d87a8028f7ce8082b510e027f2d8ca720
Vadim <stylish_bear@icloud.com>
2024-01-14 13:34:18 +0400

    add mini description into the script

--- screen.sh
+++ screen.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Created for tiling WMs
+# Deps: scrot, xclip
+# Change SCREENSHOTS_DIR to ur value
+
+SCREENSHOTS_DIR=~/images/screenshots
+
 screenshot=$(date '+%Y.%m.%d.%H.%M.%S').png
 scrot -F $screenshot
 cat $screenshot | xclip -selection clipboard -target image/png -i 
-mv $screenshot ~/images/screenshots
+mv $screenshot $SCREENSHOTS_DIR
