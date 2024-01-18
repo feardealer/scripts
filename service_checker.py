d0a4b5aaa9bee3bbcd86ca99a9d4eb4afefd4352
Vadim <stylish_bear@icloud.com>
2024-01-19 01:25:47 +0400

    docs: update desc, deps

--- service_checker.py
+++ service_checker.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python3
 
+
+"""
+Author: Vadim Belous
+Email: fancybear@internet.ru 
+
+The main goal of this script is to send notifications to the chat
+when necessary service/services is/are down on the machine where this script is running
+
+Dependencies:
+pip install python-telegram-bot
+
+Example Usage:
+python service_checker.py
+
+Systemd service example:
+
+"""
+
 import subprocess
 import asyncio
 from telegram import Bot
 
 from time import sleep
+
+"""
+1. Go to the BotFather and create bot. U will receive a token to access to the http api. Paste ur token to BOT_TOKEN
+2. Go to https://api.telegram.org/botX/getUpdates when X - is ur token. U will see chat id. Paste this value to the CHAT_ID
+
+For example, send message via curl:
+curl -X POST "https://api.telegram.org/botX/sendMessage" -d "chat_id=Y&text=Z"
+When:
+    X - Token
+    Y - Chat id
+    Z - Message
+
+"""
 
 BOT_TOKEN = ''
 CHAT_ID = ''
