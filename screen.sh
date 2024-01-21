b9b8dc1961502cc730445c596555630260e5542e
Vadim <fancybear@internet.ru>
2024-01-21 15:03:42 +0400

    feat: add arg to screenshot an area of the screen

--- screen.sh
+++ screen.sh
@@ -4,9 +4,19 @@
 # Deps: scrot, xclip
 # Change SCREENSHOTS_DIR to ur value
 
-SCREENSHOTS_DIR=~/images/screenshots
+
+# full screen ./screen.sh
+# selected area ./screen.sh -s
+
+SCREENSHOTS_DIR=$HOME/Pictures/Screenshots
 
 screenshot=$(date '+%Y.%m.%d.%H.%M.%S').png
-scrot -F $screenshot
+
+if [ "$1" == "-s" ]; then
+	scrot -s $screenshot
+else
+	scrot -F $screenshot
+fi
+
 cat $screenshot | xclip -selection clipboard -target image/png -i 
 mv $screenshot $SCREENSHOTS_DIR
